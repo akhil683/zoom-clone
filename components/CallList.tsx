5ae576a2e88b2fbf0cff4b071c9890e8ad924a67
akhil683 <onkar.palsra@gmail.com>
2024-07-03 02:22:17 +0530

    completed previous, upcoming & recordings route

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,15 +1,21 @@
+// @ts-nocheck
 "use client";
+
 import { useGetCalls } from "@/hooks/useGetCalls";
 import { Call, CallRecording } from "@stream-io/video-react-sdk";
-import { useRouter } from "next/router";
-import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 import MeetingCard from "./MeetingCard";
+import { Loader } from "lucide-react";
+import { useToast } from "./ui/use-toast";
 
 const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
+  const router = useRouter();
   const { endedCalls, upcomingCalls, callRecordings, isLoading } =
     useGetCalls();
   const [recordings, setRecordings] = useState<CallRecording[]>([]);
 
+  const { toast } = useToast();
   const getCalls = () => {
     switch (type) {
       case "ended":
@@ -34,14 +40,67 @@ const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
         return [];
     }
   };
+
+  useEffect(() => {
+    const fetchRecordings = async () => {
+      try {
+        const callData = await Promise.all(
+          callRecordings.map((meeting) => meeting.queryRecordings()),
+        );
+        const recordings = callData
+          .filter((call) => call.recordings.length > 0)
+          .flatMap((call) => call.recordings);
+        setRecordings(recordings);
+      } catch (e) {
+        toast({ title: "Try Again Later" });
+      }
+    };
+    if (type === "recordings") fetchRecordings();
+  }, [type, callRecordings]);
+
   const calls = getCalls();
   const noCallsMessage = getNoCallsMessage();
+  if (isLoading) return <Loader />;
 
   return (
     <div className="grid grid-cols-1 xl:grid-cols-2 gap-5">
       {calls && calls.length > 0 ? (
         calls.map((meeting: Call | CallRecording) => {
-          return <MeetingCard />;
+          return (
+            <MeetingCard
+              key={(meeting as Call)?.id}
+              icon={
+                type === "ended"
+                  ? "/icons/previous.svg"
+                  : type === "upcoming"
+                    ? "/icons/upcoming.svg"
+                    : "/icons/recordings.svg"
+              }
+              title={
+                (meeting as Call).state?.custom.description.substring(0, 20) ||
+                "No Description"
+              }
+              date={
+                (meeting as Call).state?.startsAt.toLocaleString() ||
+                meetin.start_time.toLocaleString()
+              }
+              isPreviousMeeting={type === "ended"}
+              buttonIcon1={
+                type === "recordings" ? "/icons/play.svg" : undefined
+              }
+              handleClick={
+                type === "recordings"
+                  ? () => router.push(`${meeting.url}`)
+                  : () => router.push(`/meeting/${meeting.id}`)
+              }
+              link={
+                type === "recordings"
+                  ? meeting.url
+                  : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meeting.id}`
+              }
+              buttonText={type === "recordings" ? "Play" : "Start"}
+            />
+          );
         })
       ) : (
         <h1>{noCallsMessage}</h1>
--- components/MeetingModal.tsx
+++ components/MeetingModal.tsx
@@ -1,14 +1,7 @@
 import React from "react";
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import Image from "next/image";
 
 interface MeetingModalProps {
@@ -35,7 +28,6 @@ const MeetingModal = ({
 }: MeetingModalProps) => {
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogTrigger>Open</DialogTrigger>
       <DialogContent className="flex w-full max-w-[520px] flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white">
         <div className="flex flex-col gap-6">
           {image && (
